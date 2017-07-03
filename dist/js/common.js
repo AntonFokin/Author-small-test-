c80c60d460204e21baf14cebf9b9e9580b24350c
AntonFokin <1antonfokin@gmail.com>
2017-07-03 23:51:47 +0300

    team-section

--- dist/js/common.js
+++ dist/js/common.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 		loop:true,
 		margin:0,
 		nav:true,
+		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
 		responsive:{
 			0:{
 				items:1
