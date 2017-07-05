6985c0e06db15a3a1198cacab5e19fd1113c96c7
AntonFokin <1antonfokin@gmail.com>
2017-07-05 17:23:13 +0300

    adaptive 1.0

--- dist/js/common.js
+++ dist/js/common.js
@@ -15,5 +15,40 @@ $(document).ready(function(){
 				items:1
 			}
 		}
-	})
+	});
+	//button hover
+		$(".button-plan").hover(function(){
+			event.preventDefault();
+			var btn= this;
+			var plan= $(btn).closest('.plan')[0];
+			var head= $(plan).children('.head')[0];
+			var h3= $(head).children('h3')[0];			
+			btn.classList.toggle('button-active');
+			h3.classList.toggle('button-active');
+		})
+	// Smooth scrolling
+	$(".nav").on("click","a", function (event) {
+	        event.preventDefault();
+	        var id  = $(this).attr('href'),
+	            top = $(id).offset().top;
+	        $('body,html').animate({scrollTop: top}, 500);
+	    });
+
+	// Fixed menu
+	var $menu = $(".topline");             
+	$(window).scroll(function(){
+		if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+			$menu.fadeOut('fast',function(){
+				$(this).removeClass("default")
+				.addClass("fixed transbg")
+				.fadeIn('fast');
+			});
+		} else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+			$menu.fadeOut('fast',function(){
+				$(this).removeClass("fixed transbg")
+				.addClass("default")
+				.fadeIn('fast');
+			});
+		}
+	});
 });
